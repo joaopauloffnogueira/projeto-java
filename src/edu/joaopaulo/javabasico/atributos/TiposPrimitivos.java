c34cd9f0258e2c0d35e27fa9a248f1683fe7beb4
joaopauloffnogueira <joaopauloffnogueira@gmail.com>
2024-08-28 19:20:26 -0300

    Foi criado a classe Tipos primitivos demonstrando todos os tipos primitivo do java e além disso a classe String

---
+++ src/edu/joaopaulo/javabasico/atributos/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package edu.joaopaulo.javabasico.atributos;
+
+public class TiposPrimitivos {
+
+    // -- Tipos primitivos para números inteiros:
+    byte numerosByte = 127;
+    short numerosCurto = 32_767;
+    int numerosInteiros = 2_147_483_647;
+    long numerosLongos = 9_223_372_036_854_775_807L;
+
+    // ---------------------------------------------
+
+    // -- Tipos primitivos para números fracionados:
+    float numerosFloat = 3.4F;
+    double numerosDouble = 21.45;
+
+    // -- Inserir textos
+    // Obs: String não é um tipo primitivo, no java ele é uma classe.
+    String nome = "João Paulo";
+
+    // -- Tipo primitivo boolean:
+    boolean verdadeiro = true;
+    boolean falso = false;
+}
