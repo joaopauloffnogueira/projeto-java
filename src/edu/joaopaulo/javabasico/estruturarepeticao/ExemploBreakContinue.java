5baf5651c181480383233485d5c03085d63630e1
joaopauloffnogueira <joaopauloffnogueira@gmail.com>
2024-09-05 19:13:58 -0300

    adicionado a pasta estrutura de repeticao com exemplos sobre while, for, foreach, dowhile, break e continue

---
+++ src/edu/joaopaulo/javabasico/estruturarepeticao/ExemploBreakContinue.java
@@ -0,0 +1,12 @@
+package edu.joaopaulo.javabasico.estruturarepeticao;
+
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for (int numero = 1; numero <= 5; numero++){
+            if (numero==3){
+                continue; //break;
+            }
+            System.out.println(numero);
+        }
+    }
+}
---
+++ src/edu/joaopaulo/javabasico/estruturarepeticao/ExemploForArray.java
@@ -0,0 +1,15 @@
+package edu.joaopaulo.javabasico.estruturarepeticao;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (int i = 0; i < alunos.length; i++){
+            System.out.println("O aluno no indice i=" + i + " é " + alunos[i]);
+        }
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
